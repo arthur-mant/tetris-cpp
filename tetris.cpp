e4582de5573ee7ad0db41fe23dabec94f0b36394
Arthur Martinelli <ama18@inf.ufpr.br>
2022-02-28 00:08:17 -0300

    classe tetris terminada o rascunho

--- tetris.cpp
+++ tetris.cpp
@@ -27,19 +27,129 @@ class Piece {
         int *image() {
             return rot_array[rotation];
         }
-        void rotate() {
-            rotation = (rotation+1) % (sizeof(rot_array)/sizeof(rot_array[0]));
+        void rotate(direction) {
+            rotation = (rotation+direction) % (sizeof(rot_array)/sizeof(rot_array[0]));
         }
-        void move_x(int n) {
+        int move_x(int n) {
             if ((n != 1) && (n != -1))
                 printf("ERROR: ILLEGAL X AXIS PIECE MOVE\n");
-            else
-                x += n;
+                return NULL;
+            x += n;
+            return x;
         }
-        void move_y(int n) {
-            if ((n != 1) && (n != -1))
-                printf("ERROR: ILLEGAL Y AXIS PIECE MOVE\n");
-            else
-                y += n;
+        int move_down(int n) {
+            y += n;
+            return y;
+        }
+/*
+        std::tuple<int, int, int> get_coordinates() {
+            return std::make_tuple(x, y, rotation);
+        }
+*/
+        int get_x() {
+            return x;
+        }
+        int get_y() {
+            return y;
+        }
+        int get_rotation() {
+            return rotation;
+        }
+}
+
+class Game {
+
+    private:
+        int score, lines, pieces, board_height, board_width;
+        Piece piece, next_piece;
+        bool gameover;
+        double fps;
+        int **field;
+    private:
+        void new_piece() {
+            piece = next_piece;
+            next_piece = Piece((board_width/2)-2, -2);
+            pieces++;
+        }
+        void break_lines() {
+            int lines = 0;
+            int holes;
+            for (int i=0; i<board_height; i++) {
+                holes = 0;
+                for (int j=0; j<board_width; j++) {
+                    if (field[i][j] == -1)
+                        holes++;
+                }
+                if (holes == 0) {
+                    lines++;
+                    for (int k=i; k<0; k--) {
+                        for (int l=0; l<board_width; l++)
+                            field[k][l] = field[k-1][l];
+                    }
+                }
+            }
+        }
+        bool intersects() {
+            bool intersection = false;
+            int i, j;
+
+            for (int block: piece.image()) {
+                i = block/4;
+                j = block%4;
+
+                if ((i+piece.get_y() >= 0) &&
+                    ((i+piece.get_y() >= board_height) ||
+                     (j+piece.get_x() >= board_width) ||
+                     (j+piece.get_x() < 0) ||
+                     (field[i+piece.get_y()][j+piece.get_x()])))
+                        intersection = true;
+            return intersection;
+        }
+        bool freeze() {
+            int i, j;
+            for (int block: piece.image()) {
+                i = block/4;
+                j = block%4;
+                field[i+piece.y][j+piece.x] = piece.type;
+            }
+            break_lines();
+            new_piece();
+            if (intersects())
+                gameover = true;
+        }
+
+    public:
+        Game(int h, int w, int **new_field) {
+            board_height = h;
+            board_width = w;
+            field = new_field;
+            score = lines = pieces = 0;
+            gameover = false;
+            fps = -1;
+            piece = Piece((board_width/2)-2, -2);
+            next_piece = Piece((board_width/2)-2, -2);
+        }
+        void hard_drop() {
+            while (!intersects())
+               piece.move_down(1); 
+            piece.move_down(-1);
+            freeze();
+        }
+        void go_down() {
+            piece.move_down(1);
+            if (intersects()) {
+                piece.move_down(-1);
+                freeze();
+            }
+        }
+        void go_side(int direction) {
+            piece.move_x(direction);
+            if (intersects())
+                piece.move_x(-direction);
+        }
+        void rotate() {
+            piece.rotate(1);
+            if (intersects())
+                piece.rotate(-1);
         }
 }
